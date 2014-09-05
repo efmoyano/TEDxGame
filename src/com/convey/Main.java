7a1cd2949b768b3b79d3fef706de591e7dc76dbf
Ernesto Moyano <ef.moyano@gmail.com>
2014-09-04 21:31:01 -0300

    add XlsProcessor

---
+++ src/com/convey/Main.java
@@ -0,0 +1,41 @@
+package com.convey;
+
+import com.convey.GUI.MainFrame;
+import com.convey.utils.XLSProcessor;
+
+/**
+ * @projectName TEDxGame
+ * @package com.convey.GUI
+ * @filename Main.java
+ * @encoding UTF-8
+ * @author Ernesto Moyano
+ * @date 04/09/2014 21:25:33
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        
+        XLSProcessor xlsproc = new XLSProcessor();
+        xlsproc.setPath("C:\\test.xls");
+        xlsproc.setSheetIndex(0);
+        xlsproc.test();
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(() -> {
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.setLocationRelativeTo(null);
+            mainFrame.setVisible(true);
+        });
+    }
+
+}
--- src/com/convey/utils/XLSProcessor.java
+++ src/com/convey/utils/XLSProcessor.java
@@ -1,7 +1,11 @@
 package com.convey.utils;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import jxl.*;
 import jxl.read.biff.BiffException;
 
@@ -15,24 +19,97 @@ import jxl.read.biff.BiffException;
  */
 public class XLSProcessor {
 
-    public void test() throws IOException, BiffException {
+    private String f_path;
 
-        Workbook workbook = Workbook.getWorkbook(new File("C:\\test.xls"));
+    private int f_sheetIndex;
 
-        Sheet sheet = workbook.getSheet(0);
+    public static final String PROP_SHEETINDEX = "sheetIndex";
 
-        Cell a1 = sheet.getCell(0, 0);
-        Cell b2 = sheet.getCell(0, 1);
-        Cell c2 = sheet.getCell(0, 2);
-
-        String stringa1 = a1.getContents();
-        String stringb2 = b2.getContents();
-        String stringc2 = c2.getContents();
-
-        System.out.println(stringa1);
-        System.out.println(stringb2);
-        System.out.println(stringc2);
-
+    /**
+     * Get the value of f_sheetIndex
+     *
+     * @return the value of f_sheetIndex
+     */
+    public int getSheetIndex() {
+        return f_sheetIndex;
     }
 
+    /**
+     * Set the value of f_sheetIndex
+     *
+     * @param sheetIndex new value of f_sheetIndex
+     */
+    public void setSheetIndex(int sheetIndex) {
+        int oldSheetIndex = this.f_sheetIndex;
+        this.f_sheetIndex = sheetIndex;
+        propertyChangeSupport.firePropertyChange(PROP_SHEETINDEX, oldSheetIndex, sheetIndex);
+    }
+
+    public static final String PROP_PATH = "path";
+
+    /**
+     * Get the value of f_path
+     *
+     * @return the value of f_path
+     */
+    public String getPath() {
+        return f_path;
+    }
+
+    /**
+     * Set the value of f_path
+     *
+     * @param path new value of f_path
+     */
+    public void setPath(String path) {
+        String oldPath = this.f_path;
+        this.f_path = path;
+        propertyChangeSupport.firePropertyChange(PROP_PATH, oldPath, path);
+    }
+
+    private transient final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    /**
+     * Add PropertyChangeListener.
+     *
+     * @param listener
+     */
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    /**
+     * Remove PropertyChangeListener.
+     *
+     * @param listener
+     */
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+
+    public void test() {
+        File l_fileSheet = new File(f_path);;
+        Workbook l_workbook = null;
+        Sheet l_sheet;
+        try {
+            l_workbook = Workbook.getWorkbook(l_fileSheet);
+        } catch (IOException | BiffException ex) {
+            System.out.println("Error loading file");
+            Logger.getLogger(XLSProcessor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (l_workbook != null) {
+            l_sheet = l_workbook.getSheet(f_sheetIndex);
+            Cell a1 = l_sheet.getCell(0, 0);
+            Cell b2 = l_sheet.getCell(0, 1);
+            Cell c2 = l_sheet.getCell(0, 2);
+
+            String stringa1 = a1.getContents();
+            String stringb2 = b2.getContents();
+            String stringc2 = c2.getContents();
+
+            System.out.println(stringa1);
+            System.out.println(stringb2);
+            System.out.println(stringc2);
+        }
+    }
 }
