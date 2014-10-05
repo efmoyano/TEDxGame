97f11730eb022d3af796eaeb7b5436815a55d04c
Ernesto Moyano <ef.moyano@gmail.com>
2014-10-05 16:48:41 -0300

    cool UI :)

--- src/com/convey/GUI/GameMainPanel.java
+++ src/com/convey/GUI/GameMainPanel.java
@@ -3,12 +3,14 @@ package com.convey.GUI;
 import com.convey.game.GameEngine;
 import com.convey.game.Question;
 import com.convey.utils.Animation;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.border.LineBorder;
 
 /**
  *
@@ -22,7 +24,54 @@ public final class GameMainPanel extends javax.swing.JPanel {
     private transient final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
     private MainFrame mainFrame;
     public static final String PROP_MAINFRAME = "mainFrame";
-    Dimension l_panelSize;
+    private Dimension l_panelSize;
+
+    private Color answerResult = Color.WHITE;
+    public static final String PROP_ANSWERRESULT = "answerResult";
+
+    private LineBorder answerBorder;
+
+    public static final String PROP_ANSWERBORDER = "answerBorder";
+
+    /**
+     * Get the value of answerBorder
+     *
+     * @return the value of answerBorder
+     */
+    public LineBorder getAnswerBorder() {
+        return answerBorder;
+    }
+
+    /**
+     * Set the value of answerBorder
+     *
+     * @param answerBorder new value of answerBorder
+     */
+    public void setAnswerBorder(LineBorder answerBorder) {
+        LineBorder oldAnswerBorder = this.answerBorder;
+        this.answerBorder = answerBorder;
+        propertyChangeSupport.firePropertyChange(PROP_ANSWERBORDER, oldAnswerBorder, answerBorder);
+    }
+
+    /**
+     * Get the value of answerResult
+     *
+     * @return the value of answerResult
+     */
+    public Color getAnswerResult() {
+        return answerResult;
+    }
+
+    /**
+     * Set the value of answerResult
+     *
+     * @param answerResult new value of answerResult
+     */
+    public void setAnswerResult(Color answerResult) {
+        Color oldAnswerResult = this.answerResult;
+        this.answerResult = answerResult;
+        propertyChangeSupport.firePropertyChange(PROP_ANSWERRESULT, oldAnswerResult, answerResult);
+    }
 
     /**
      * Get the value of mainFrame
@@ -99,6 +148,9 @@ public final class GameMainPanel extends javax.swing.JPanel {
     }
 
     public void loadNextQuestion() {
+
+        hideAnswers(false, false, false, false);
+
         this.lblMensajeRespuesta.setText("");
         this.currentQuestion = this.gameEngine.getNextQuestion();
 
@@ -110,18 +162,45 @@ public final class GameMainPanel extends javax.swing.JPanel {
         this.lblRedOption.setText(this.currentQuestion.getOptionRed());
 
         Rectangle l_redFrom = new Rectangle(l_panelSize.width / 4, l_panelSize.height / 2, 0, 0);
-//        Rectangle l_orangeFrom = new Rectangle(l_panelSize.height, l_panelSize.width, l_panelSize.width / 2, l_panelSize.height / 2);
-//        Rectangle l_greenFrom = new Rectangle(l_panelSize.width, l_panelSize.height, l_panelSize.width / 2, l_panelSize.height / 2);
-//        Rectangle l_yellowFrom = new Rectangle(0, l_panelSize.height, l_panelSize.width / 2, l_panelSize.height / 2);
+
+        Rectangle l_orangeFrom = new Rectangle(l_panelSize.width / 4, l_panelSize.height / 2, 0, 0);
+
+        Rectangle l_greenFrom = new Rectangle(l_panelSize.width / 2, l_panelSize.height, 0, 0);
+
+        Rectangle l_yellowFrom = new Rectangle(l_panelSize.width / 2, l_panelSize.height, 0, 0);
 
         Rectangle to = new Rectangle(0, 0, l_panelSize.width / 2, l_panelSize.height / 4);
-        //(0,0,H,W)
 
-        new Animation(redAnswer, l_redFrom, to).start();
-//        new Animation(orangeAnswer, l_orangeFrom, to).start();
-//        new Animation(greenAnswer, l_greenFrom, to).start();
-//        new Animation(yellowAnswer, l_yellowFrom, to).start();
+        new Thread(() -> {
+            try {
+                hideAnswers(true, false, false, false);
+                new Animation(redAnswer, l_redFrom, to).start();
 
+                Thread.sleep(250);
+
+                hideAnswers(true, true, false, false);
+                new Animation(orangeAnswer, l_orangeFrom, to).start();
+
+                Thread.sleep(250);
+
+                hideAnswers(true, true, true, false);
+                new Animation(yellowAnswer, l_yellowFrom, to).start();
+
+                Thread.sleep(250);
+
+                hideAnswers(true, true, true, true);
+                new Animation(greenAnswer, l_greenFrom, to).start();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(GameMainPanel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }).start();
+    }
+
+    public void hideAnswers(boolean p_flag1, boolean p_flag2, boolean p_flag3, boolean p_flag4) {
+        redAnswer.setVisible(p_flag1);
+        orangeAnswer.setVisible(p_flag2);
+        yellowAnswer.setVisible(p_flag3);
+        greenAnswer.setVisible(p_flag4);
     }
 
     public void checkResponse(String p_resopnse) {
@@ -129,14 +208,22 @@ public final class GameMainPanel extends javax.swing.JPanel {
         new Thread(() -> {
             if (currentQuestion.evalResponse(p_resopnse)) {
                 lblMensajeRespuesta.setText("Respuesta Correcta!!!");
+                setAnswerResult(Color.GREEN);
+                setAnswerBorder(new javax.swing.border.LineBorder(Color.GREEN, 10, true));
             } else {
                 lblMensajeRespuesta.setText("Respuesta Incorrecta!!!");
+                setAnswerResult(Color.RED);
+                setAnswerBorder(new javax.swing.border.LineBorder(Color.RED, 10, true));
             }
+
             try {
                 Thread.sleep(3000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(GameMainPanel.class.getName()).log(Level.SEVERE, null, ex);
             }
+            hideAnswers(false, false, false, false);
+            setAnswerResult(Color.WHITE);
+            setAnswerBorder(null);
             loadNextQuestion();
         }).start();
     }
@@ -162,7 +249,9 @@ public final class GameMainPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
         java.awt.GridBagConstraints gridBagConstraints;
+        bindingGroup = new org.jdesktop.beansbinding.BindingGroup();
 
+        customPanel1 = new com.convey.component.CustomPanel();
         questionPanel = new javax.swing.JPanel();
         lblQuestion = new javax.swing.JLabel();
         answersPanel = new javax.swing.JPanel();
@@ -181,16 +270,23 @@ public final class GameMainPanel extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
 
-        setBackground(new java.awt.Color(204, 204, 204));
+        setBackground(new java.awt.Color(204, 255, 255));
         setForeground(new java.awt.Color(204, 0, 153));
         setMinimumSize(new java.awt.Dimension(640, 480));
         setPreferredSize(new java.awt.Dimension(640, 480));
+
+        org.jdesktop.beansbinding.Binding binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, this, org.jdesktop.beansbinding.ELProperty.create("${answerBorder}"), this, org.jdesktop.beansbinding.BeanProperty.create("border"));
+        bindingGroup.addBinding(binding);
+
         addComponentListener(new java.awt.event.ComponentAdapter() {
             public void componentResized(java.awt.event.ComponentEvent evt) {
                 formComponentResized(evt);
             }
         });
         setLayout(new java.awt.BorderLayout());
+
+        customPanel1.setImageBackground("wallpaper.png");
+        customPanel1.setLayout(new java.awt.BorderLayout(10, 10));
 
         questionPanel.setOpaque(false);
         java.awt.GridBagLayout questionPanelLayout = new java.awt.GridBagLayout();
@@ -199,8 +295,13 @@ public final class GameMainPanel extends javax.swing.JPanel {
         questionPanel.setLayout(questionPanelLayout);
 
         lblQuestion.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        lblQuestion.setForeground(new java.awt.Color(255, 255, 255));
         lblQuestion.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lblQuestion.setText("Como se llama la version anterior a windows XP ?");
+
+        binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, this, org.jdesktop.beansbinding.ELProperty.create("${answerResult}"), lblQuestion, org.jdesktop.beansbinding.BeanProperty.create("foreground"));
+        bindingGroup.addBinding(binding);
+
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
@@ -208,12 +309,12 @@ public final class GameMainPanel extends javax.swing.JPanel {
         gridBagConstraints.insets = new java.awt.Insets(30, 30, 30, 30);
         questionPanel.add(lblQuestion, gridBagConstraints);
 
-        add(questionPanel, java.awt.BorderLayout.PAGE_START);
+        customPanel1.add(questionPanel, java.awt.BorderLayout.PAGE_START);
 
-        answersPanel.setLayout(new java.awt.GridLayout(2, 2, 1, 1));
+        answersPanel.setOpaque(false);
+        answersPanel.setLayout(new java.awt.GridLayout(2, 2, 30, 30));
 
         redAnswer.setBackground(new java.awt.Color(255, 0, 51));
-        redAnswer.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(204, 204, 204)));
         redAnswer.setLayout(new java.awt.GridBagLayout());
 
         lblRedOption.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
@@ -228,7 +329,6 @@ public final class GameMainPanel extends javax.swing.JPanel {
         answersPanel.add(redAnswer);
 
         orangeAnswer.setBackground(new java.awt.Color(255, 102, 0));
-        orangeAnswer.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(204, 204, 204)));
         orangeAnswer.setLayout(new java.awt.GridBagLayout());
 
         lblOrangeOption.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
@@ -243,7 +343,6 @@ public final class GameMainPanel extends javax.swing.JPanel {
         answersPanel.add(orangeAnswer);
 
         yellowAnswer.setBackground(new java.awt.Color(255, 255, 51));
-        yellowAnswer.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(204, 204, 204)));
         yellowAnswer.setLayout(new java.awt.GridBagLayout());
 
         lblYellowOption.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
@@ -258,7 +357,6 @@ public final class GameMainPanel extends javax.swing.JPanel {
         answersPanel.add(yellowAnswer);
 
         greenAnswer.setBackground(new java.awt.Color(0, 153, 102));
-        greenAnswer.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(204, 204, 204)));
         greenAnswer.setLayout(new java.awt.GridBagLayout());
 
         lblGreenOption.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
@@ -272,14 +370,20 @@ public final class GameMainPanel extends javax.swing.JPanel {
 
         answersPanel.add(greenAnswer);
 
-        add(answersPanel, java.awt.BorderLayout.CENTER);
+        customPanel1.add(answersPanel, java.awt.BorderLayout.CENTER);
 
+        jPanel2.setOpaque(false);
         jPanel2.setLayout(new java.awt.GridBagLayout());
 
+        informationPanel.setOpaque(false);
         informationPanel.setLayout(new java.awt.GridBagLayout());
 
         lblMensajeRespuesta.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         lblMensajeRespuesta.setText("Respuesta Correcta!!");
+
+        binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, this, org.jdesktop.beansbinding.ELProperty.create("${answerResult}"), lblMensajeRespuesta, org.jdesktop.beansbinding.BeanProperty.create("foreground"));
+        bindingGroup.addBinding(binding);
+
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
@@ -338,16 +442,21 @@ public final class GameMainPanel extends javax.swing.JPanel {
         gridBagConstraints.insets = new java.awt.Insets(20, 20, 20, 20);
         jPanel2.add(conveyBrand, gridBagConstraints);
 
-        add(jPanel2, java.awt.BorderLayout.PAGE_END);
+        customPanel1.add(jPanel2, java.awt.BorderLayout.PAGE_END);
+
+        add(customPanel1, java.awt.BorderLayout.CENTER);
+
+        bindingGroup.bind();
     }// </editor-fold>//GEN-END:initComponents
 
     private void formComponentResized(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_formComponentResized
-        
+
     }//GEN-LAST:event_formComponentResized
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel answersPanel;
     private javax.swing.JPanel conveyBrand;
+    private com.convey.component.CustomPanel customPanel1;
     private javax.swing.JPanel greenAnswer;
     private javax.swing.JPanel informationPanel;
     private javax.swing.JLabel jLabel1;
@@ -363,5 +472,6 @@ public final class GameMainPanel extends javax.swing.JPanel {
     private javax.swing.JPanel questionPanel;
     private javax.swing.JPanel redAnswer;
     private javax.swing.JPanel yellowAnswer;
+    private org.jdesktop.beansbinding.BindingGroup bindingGroup;
     // End of variables declaration//GEN-END:variables
 }
